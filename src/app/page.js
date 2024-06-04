55b91f159a881eb1f1e523c375fd8a163d19fbbb
zermzerm <zerm0907@gmail.com>
2024-06-04 18:05:33 +0900

    기본 스타일 설정 및 Scroll Snap 적용

--- src/app/page.js
+++ src/app/page.js
@@ -2,15 +2,26 @@ import About from "./About";
 import Contact from "./Contact";
 import Projects from "./Projects";
 import Skills from "./Skills";
+import styles from "./styles.module.css";
 
 export default function Home() {
   return (
-    <>
-      <section>Home</section>
-      <About />
-      <Skills />
-      <Projects />
-      <Contact />
-    </>
+    <div className={styles.scrollContainer}>
+      <div className={styles.scrollArea} id="home">
+        Home
+      </div>
+      <div className={styles.scrollArea} id="about">
+        <About />
+      </div>
+      <div className={styles.scrollArea} id="skills">
+        <Skills />
+      </div>
+      <div className={styles.scrollArea} id="projects">
+        <Projects />
+      </div>
+      <div className={styles.scrollArea} id="contact">
+        <Contact />
+      </div>
+    </div>
   );
 }
