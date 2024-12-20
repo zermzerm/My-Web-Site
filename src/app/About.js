a0d5a8d184b1193439c1354a5b92c144ef0801d7
zermzerm <zerm0907@gmail.com>
2024-12-20 14:40:49 +0900

    About 틀 잡기

--- src/app/About.js
+++ src/app/About.js
@@ -1,5 +1,16 @@
 import sectionStyles from "./styles/sectionStyles.module.css";
 
 export default function About() {
-  return <section className={sectionStyles}>About</section>;
+  return (
+    <section className={sectionStyles.about}>
+      <div>
+        <p>이름</p>
+        <p>박경서</p>
+      </div>
+      <div>
+        <p>생년월일</p>
+        <p>1996.09.07</p>
+      </div>
+    </section>
+  );
 }
