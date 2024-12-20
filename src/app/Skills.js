beaae65114df80741c68ecf265b5f496adfd3b91
zermzerm <zerm0907@gmail.com>
2024-12-20 14:55:15 +0900

    skills 틀 잡기

--- src/app/Skills.js
+++ src/app/Skills.js
@@ -1,3 +1,24 @@
+import sectionStyles from "./styles/sectionStyles.module.css";
+
 export default function Skills() {
-  return <section>Skills</section>;
+  return (
+    <section className={sectionStyles.skills}>
+      <div className={sectionStyles.skillsContainer}>
+        skills
+        <hr className={sectionStyles.customHr} />
+        <div>
+          <p>Html</p>
+          <p>Css</p>
+          <p>StyledComponent</p>
+          <p>CssModule</p>
+          <p>React</p>
+          <p>ReactQuery</p>
+          <p>TypeCcript</p>
+          <p>NextJS</p>
+          <p>Recoil</p>
+          <p>Zustand</p>
+        </div>
+      </div>
+    </section>
+  );
 }
