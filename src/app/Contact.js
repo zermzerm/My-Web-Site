753206fe2454d256a4c3873ea4186f35ba82a5c0
zermzerm <zerm0907@gmail.com>
2024-12-28 23:07:15 +0900

    Contact 페이지 틀 구현

--- src/app/Contact.js
+++ src/app/Contact.js
@@ -2,8 +2,23 @@ import contactStyles from "./styles/sectionStyles.module.css";
 
 export default function Contact() {
   return (
-    <section>
-      <div></div>
+    <section className={contactStyles.contactContainer}>
+      <div className={contactStyles.contactMailWrapper}>
+        <p>mail</p>
+        <p>zerm0907@gmail.com</p>
+      </div>
+      <div className={contactStyles.contactGithubWrapper}>
+        <p>github</p>
+        <p>https://github.com/zermzerm</p>
+      </div>
+      <div className={contactStyles.contactTistoryWrapper}>
+        <p>tistory</p>
+        <p>https://zermzerm.tistory.com/</p>
+      </div>
+      <div className={contactStyles.contactTellWrapper}>
+        <p>tell</p>
+        <p>01048715869</p>
+      </div>
     </section>
   );
 }
