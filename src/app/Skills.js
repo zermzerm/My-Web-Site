4043d3cf7e11dddb5d1bcf127eba55a83f5d40f5
zermzerm <zerm0907@gmail.com>
2025-01-14 16:49:47 +0900

    skills 부분 디자인

--- src/app/Skills.js
+++ src/app/Skills.js
@@ -6,64 +6,108 @@ export default function Skills() {
     <main className={sectionStyles.skills}>
       <section className={sectionStyles.skillsContainer}>
         <h1 className={sectionStyles.skillsTitle}>Skills</h1>
-        {/* <hr className={sectionStyles.customHr} /> */}
         <article className={sectionStyles.skillsWrapper}>
-          <Image src="/images/HTML.svg" width={70} height={70} alt="HTML" />
-          <Image src="/images/CSS.svg" width={70} height={70} alt="CSS" />
-          <Image
-            src="/images/StyledComponents.svg"
-            width={70}
-            height={70}
-            alt="StyledComponents"
-          />
-          <Image src="/images/React.svg" width={70} height={70} alt="React" />
-          <Image
-            src="/images/TypeScript.svg"
-            width={70}
-            height={70}
-            alt="TypeScript"
-          />
-          <Image
-            src="/images/NextJS-Dark.svg"
-            width={70}
-            height={70}
-            alt="NextJS-Dark"
-          />
-          <Image src="/images/Figma.svg" width={70} height={70} alt="Figma" />
-          <Image src="/images/Git.svg" width={70} height={70} alt="Git" />
-          <Image src="/images/Github.svg" width={70} height={70} alt="Github" />
-          <Image
-            src="/images/ReactQuery.svg"
-            width={70}
-            height={70}
-            alt="ReactQuery"
-          />
-          <Image
-            src="/images/Zustand.svg"
-            width={70}
-            height={70}
-            alt="Zustand"
-          />
-          <Image
-            src="/images/Recoil.svg"
-            width={70}
-            height={70}
-            alt="Recoil"
-            className={sectionStyles.skillsImage}
-          />
-          <Image
-            src="/images/Netlify.svg"
-            width={70}
-            height={70}
-            alt="Netlify"
-          />
-          <Image src="/images/Vercel.svg" width={70} height={70} alt="Vercel" />
-          <Image
-            src="/images/CSSModule.png"
-            width={70}
-            height={70}
-            alt="CSSModule"
-          />
+          {/* 기술 스택 */}
+          <section className={sectionStyles.skillsSection}>
+            <h2 className={sectionStyles.skillsSubTitle}>Technology Stack</h2>
+
+            {/* 언어 및 프레임워크 */}
+            <div className={sectionStyles.skillsListContainer}>
+              <h3>Languages & Frameworks</h3>
+              <ul className={sectionStyles.skillsList}>
+                <li>
+                  <Image src="/images/HTML.svg" width={70} height={70} alt="HTML" />
+                </li>
+                <li>
+                  <Image src="/images/TypeScript.svg" width={70} height={70} alt="TypeScript" />
+                </li>
+                <li>
+                  <Image src="/images/NextJS-Dark.svg" width={70} height={70} alt="NextJS-Dark" />
+                </li>
+              </ul>
+            </div>
+
+            {/* 상태 관리 및 라이브러리 */}
+            <div className={sectionStyles.skillsListContainer}>
+              <h3>Library & State Management</h3>
+              <ul className={sectionStyles.skillsList}>
+                <li>
+                  <Image src="/images/React.svg" width={70} height={70} alt="React" />
+                </li>
+                <li>
+                  <Image src="/images/ReactQuery.svg" width={70} height={70} alt="ReactQuery" />
+                </li>
+                <li>
+                  <Image src="/images/Zustand.svg" width={70} height={70} alt="Zustand" />
+                </li>
+                <li>
+                  <Image
+                    src="/images/Recoil.svg"
+                    width={70}
+                    height={70}
+                    alt="Recoil"
+                    className={sectionStyles.skillsImage}
+                  />
+                </li>
+              </ul>
+            </div>
+
+            {/* 스타일링 */}
+            <div className={sectionStyles.skillsListContainer}>
+              <h3>Styling</h3>
+              <ul className={sectionStyles.skillsList}>
+                <li>
+                  <Image src="/images/CSS.svg" width={70} height={70} alt="CSS" />
+                </li>
+                <li>
+                  <Image
+                    src="/images/StyledComponents.svg"
+                    width={70}
+                    height={70}
+                    alt="StyledComponents"
+                  />
+                </li>
+                <li>
+                  <Image src="/images/CSSModule.png" width={70} height={70} alt="CSSModule" />
+                </li>
+                <li>
+                  <Image src="/images/Figma.svg" width={70} height={70} alt="Figma" />
+                </li>
+              </ul>
+            </div>
+          </section>
+
+          <section className={sectionStyles.skillsUtilities}>
+            {/* 버젼 관리 */}
+            <article className={sectionStyles.skillsSection}>
+              <div className={sectionStyles.skillsListContainer}>
+                <h2 className={sectionStyles.skillsSubTitle}>Version Control</h2>
+                <ul className={sectionStyles.skillsList}>
+                  <li>
+                    <Image src="/images/Git.svg" width={70} height={70} alt="Git" />
+                  </li>
+                  <li>
+                    <Image src="/images/Github.svg" width={70} height={70} alt="Github" />
+                  </li>
+                </ul>
+              </div>
+            </article>
+
+            {/* 배포 */}
+            <article className={sectionStyles.skillsSection}>
+              <div className={sectionStyles.skillsListContainer}>
+                <h2 className={sectionStyles.skillsSubTitle}>Deployment</h2>
+                <ul className={sectionStyles.skillsList}>
+                  <li>
+                    <Image src="/images/Netlify.svg" width={70} height={70} alt="Netlify" />
+                  </li>
+                  <li>
+                    <Image src="/images/Vercel.svg" width={70} height={70} alt="Vercel" />
+                  </li>
+                </ul>
+              </div>
+            </article>
+          </section>
         </article>
       </section>
     </main>
